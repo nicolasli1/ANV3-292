de908846563767583d875b908792913f99024f57
Alexandra Baquero <alexandra.baquero@csgi.com>
2024-10-11 08:45:24 -0500

    Taller 1 servidor web apache

--- infra/vpc/app.py
+++ infra/vpc/app.py
@@ -1,12 +1,15 @@
 
-from os import path
-import os.path
+from os import path # Se utiliza para manejar rutas de archivos
+import os.path 
 import aws_cdk as cdk
+#import aws_cdk.aws_s3_assets import Asset # Se utilizar para trabajar con activos de s3
 
-from constructs import Construct
+from constructs import Construct # Se utiliza como clase base para los constructores de CDK
+
 
 from aws_cdk import(
     aws_ec2 as ec2,
+    aws_iam as iam,
     App,
     Stack
 )
@@ -14,12 +17,12 @@ from aws_cdk import(
 class Ec2VpcStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+        super().__init__(scope, id, **kwargs) #Constructor clase base stack
 
+        #Crea una nueva vpc con nat_gateways=0 sin puertas de enlace NAT y se define una configuracion de subredes con una subred publica
         vpc = ec2.Vpc(
             self,
-            id="MyVpcClass",
-            max_azs=3,
+            id="MyVpcTalle1",
             nat_gateways=0,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
@@ -29,7 +32,52 @@ class Ec2VpcStack(Stack):
                ),
             ]
         )
+        
+        # AMI
+        # Se selecciona la ultima version de Amazon Linux 2 
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
+
+        # Instance Role and SSM Managed Policy
+        # Crea un rol para la instancia EC2
+        #Se asigna a EC2
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+        #Se agrega la politica de SSM Managed instance Core
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            "sudo yum update -y",
+            "sudo yum install -y httpd",
+            "sudo systemctl start httpd",
+            "sudo systemctl enable httpd",
+            "sudo echo 'Hola Mundo!' > /var/www/html/index.html"
+        )
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            vpc = vpc,
+            machine_image=amzn_linux,
+            role=role,
+            user_data=user_data
+            )
+        
+        instance.connections.allow_from(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(80)
+        )
+
+        instance.connections.allow_from(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(22)
+        )
+        
 app = App()
-Ec2VpcStack(app, "vpc")
+Ec2VpcStack(app, "ec2-instance")
 
 app.synth()
