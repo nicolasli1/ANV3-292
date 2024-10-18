2193f447c7c95693adaf26a525ce6f31deee5a52
Nicolas Linares <ing.nicolas.linares@gmail.com>
2024-10-18 08:46:00 -0500

    Creacion de lambda y base de datos en dynamodb

--- infra/cdk/app.py
+++ infra/cdk/app.py
@@ -9,6 +9,7 @@ from aws_cdk import (
     aws_iam as iam,
     aws_apigateway as api_g,
     aws_lambda as lb,
+    aws_dynamodb as dynamodb,
     App,
     Stack,
 )
@@ -32,18 +33,22 @@ class Ec2VpcStack(Stack):
         )
 
         # AMI
-        # Se selecciona la ultima version de Amazon Linux 2 
+        # Se selecciona la ultima version de Amazon Linux 2
         amzn_linux = ec2.MachineImage.latest_amazon_linux(
             generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
             edition=ec2.AmazonLinuxEdition.STANDARD,
             virtualization=ec2.AmazonLinuxVirt.HVM,
-            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+        )
+
+        role = iam.Role(
+            self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")
+        )
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                "AmazonSSMManagedInstanceCore"
             )
-
-        
-        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
-        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
-
+        )
 
         user_data = ec2.UserData.for_linux()
 
@@ -52,27 +57,23 @@ class Ec2VpcStack(Stack):
             "sudo yum install -y httpd",
             "sudo systemctl start httpd",
             "sudo systemctl enable httpd",
-            "sudo echo 'Hola Mundo!' > /var/www/html/index.html"
+            "sudo echo 'Hola Mundo!' > /var/www/html/index.html",
         )
 
-
-        instance = ec2.Instance(self, "Instance",
+        instance = ec2.Instance(
+            self,
+            "Instance",
             instance_type=ec2.InstanceType("t2.micro"),
-            vpc = vpc,
+            vpc=vpc,
             machine_image=amzn_linux,
             role=role,
-            user_data=user_data
-            )
-        
-        instance.connections.allow_from(
-            ec2.Peer.any_ipv4(),
-            ec2.Port.tcp(80)
+            user_data=user_data,
         )
 
-        instance.connections.allow_from(
-            ec2.Peer.any_ipv4(),
-            ec2.Port.tcp(22)
-        )
+        instance.connections.allow_from(ec2.Peer.any_ipv4(), ec2.Port.tcp(80))
+
+        instance.connections.allow_from(ec2.Peer.any_ipv4(), ec2.Port.tcp(22))
+
 
 class MiPrimerAPI(Stack):
 
@@ -88,15 +89,44 @@ class MiPrimerAPI(Stack):
             runtime=lb.Runtime.PYTHON_3_12,
         )
 
+        fn_get_items_table = lb.Function(
+            self,
+            id="Fn_get_items_table",
+            handler="lambda_get_items_code.lambda_handler",
+            code=lb.Code.from_asset("../../lambda/Fn_get_menu_code"),
+            timeout=Duration.seconds(60),
+            runtime=lb.Runtime.PYTHON_3_12,
+        )
+
+        global_table = dynamodb.TableV2(
+            self,
+            id="GlobalTable",
+            table_name="My_primera_global_table_292",
+            billing=dynamodb.Billing.on_demand(),
+            partition_key=dynamodb.Attribute(
+                name="id_pk", type=dynamodb.AttributeType.STRING
+            ),
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+        )
+
+        global_table.grant_full_access(fn_get_items_table)
+
         api_1 = api_g.RestApi(self, id="Class-229", rest_api_name="Api-Anv3-292")
 
         menu_resource = api_1.root.add_resource("menu")
+        items_table = api_1.root.add_resource("items")
 
         integration_fn_get_menu = api_g.LambdaIntegration(fn_get_menu)
+        integration_fn_get_items = api_g.LambdaIntegration(fn_get_items_table)
 
         menu_resource.add_method(
             "GET",
             integration_fn_get_menu,
+        )
+
+        items_table.add_method(
+            "GET",
+            integration_fn_get_items,
         )
 
 
