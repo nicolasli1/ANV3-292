b29d8ab1c64e75e79d2b59bb357d273cc0cdde3c
Nicolas Linares <ing.nicolas.linares@gmail.com>
2024-11-06 06:48:06 -0500

    update expose port

--- docker/server.mjs
+++ docker/server.mjs
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http';
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
