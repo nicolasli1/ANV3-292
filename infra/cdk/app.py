15d23314cab2f677b3611c126546172a2c44f826
Nicolas Linares <ing.nicolas.linares@gmail.com>
2024-11-07 09:03:49 -0500

    ecs

--- infra/cdk/app.py
+++ infra/cdk/app.py
@@ -3,8 +3,13 @@ import os.path
 
 import aws_cdk as cdk
 
+
+
 from constructs import Construct
 from aws_cdk import Duration, RemovalPolicy
+import aws_cdk.aws_stepfunctions as sfn
+import aws_cdk.aws_ecs_patterns as ecs_patterns
+import aws_cdk.aws_stepfunctions_tasks as tasks
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_iam as iam,
@@ -15,6 +20,8 @@ from aws_cdk import (
     aws_cloudfront_origins as origins,
     aws_s3 as s3,
     aws_s3_deployment as s3_deployment,
+    aws_ecs as ecs,
+    aws_logs as logs,
     App,
     Stack,
 )
@@ -139,7 +146,6 @@ class MiPrimerAPI(Stack):
         items_table.add_method("GET", integration_fn_get_items)
         items_table.add_method("POST", integration_fn_post_items)
 
-
         items_table.add_method(
             "OPTIONS",
             api_g.MockIntegration(
@@ -167,6 +173,7 @@ class MiPrimerAPI(Stack):
                 )
             ],
         )
+
 
 class MyWebsiteS3(Stack):
 
@@ -214,9 +221,57 @@ class MyWebsiteS3(Stack):
         )
 
 
+class ecsCluster(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Crear una VPC
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
+
+
+        security_group = ec2.SecurityGroup(self, "ecs-fargte",
+            vpc=vpc,
+            description="Allow HTTP traffic on port 80",
+            allow_all_outbound=True  # Permitir todas las salidas
+        )
+        # Agregar una regla para permitir tráfico HTTP en el puerto 80 desde cualquier IP
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),  # Acepta tráfico desde cualquier IP (0.0.0.0/0)
+            ec2.Port.tcp(80),  # Permitir tráfico en el puerto TCP 80 (HTTP)
+            "Allow HTTP traffic"
+        )
+
+        # Crear un cluster ECS
+        cluster = ecs.Cluster(self, "EcsCluster", vpc=vpc)
+
+        # Crear la definición de la tarea ECS
+        task_definition = ecs.FargateTaskDefinition(self, "TaskDef",
+            memory_limit_mib=512,  # 512 MB
+            cpu=256,  # 0.25 vCPU
+        )
+
+        # Agregar un contenedor a la definición de tarea
+        container = task_definition.add_container("AppContainer",
+            image=ecs.ContainerImage.from_registry("nicolasli1/image292:latest"),
+            logging=ecs.LogDrivers.aws_logs(stream_prefix="myapp", log_retention=logs.RetentionDays.ONE_WEEK),
+            #port_mappings=[ecs.PortMapping(container_port=3000)],
+            environment={"PORT":"80"},
+        )
+
+        # Crear el servicio ECS
+        ecs.FargateService(self, "FargateService",
+            cluster=cluster,
+            task_definition=task_definition,
+            desired_count=1,  # Número de instancias del contenedor a ejecutar
+            assign_public_ip=True,  # Asigna una IP pública para poder acceder desde Internet
+            security_groups=[security_group]
+        )
+
 app = App()
 Ec2VpcStack(app, "vpc")
 MiPrimerAPI(app, "mi-api")
 MyWebsiteS3(app, "my-website-s3")
+ecsCluster(app, "ecs-fargate")
 
 app.synth()
